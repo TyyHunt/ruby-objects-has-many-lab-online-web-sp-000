c55d3a0e824b414ccb8158019751e261d6b3280e
Tyler Hunt <tyhunt04@gmail.com>
2019-02-26 16:51:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 class Artist
   attr_accessor :name
 
+  @@song_count = 0
+
   def initialize(name)
     @name = name
     @songs = []
@@ -12,15 +14,19 @@ class Artist
 
   def add_song(song)
     @songs << song
+    song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(song)
-    song.new = song
+    song = Song.new(name)
     @songs << song
+    song.artist = self
+    @@song_count += 1
   end
 
   def self.song_count
-    return @songs.size
+    @@song_count
   end
 
 
