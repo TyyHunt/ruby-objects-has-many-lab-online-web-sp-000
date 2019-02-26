3c1a890bed151884d563f98a007bd7862c5aef6a
Tyler Hunt <tyhunt04@gmail.com>
2019-02-26 16:24:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,4 +3,20 @@ class Artist
 
   def initialize(name)
     @name = name
+    @songs = []
+  end
+
+  def add_song(title)
+    @songs << title
+  end
+
+  def add_song_by_name(title)
+    title.name = song
+    @songs << song
+  end
+
+  def song_count
+    @songs.size
+  end
+  
 end
